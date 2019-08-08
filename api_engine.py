42b94afb4d523a67f6bc19c3f6846d62afc3fa1f
Neung0 <Neung0@users.noreply.github.com>
2019-08-08 14:49:16 +0900

    modify error func

--- api_engine.py
+++ api_engine.py
@@ -27,20 +27,15 @@ def bookInfo(title):
     url = "https://openapi.naver.com/v1/search/book?query=" + title + "&display=5&sort=count"
     headers={"X-Naver-Client-Id":clientId, "X-Naver-Client-Secret": clientSecret}
     res = requests.get(url, headers=headers)
-    resCode = res.status_code
-    if resCode == 200:
-        search = res.json()
-        book = search['items']
-        for i in book:
-            t = i['title']
-            t = t.replace('<b>', '')
-            t = t.replace('</b>', '')
-            i['title'] = t
-            i['isbn'] = i['isbn'][11:]
-        return book
-    else:
-        print("Error Code:" + resCode)
-        # res.raise_for_status()  200 아니면 오류
+    search = res.json()
+    book = search['items']
+    for i in book:
+        t = i['title']
+        t = t.replace('<b>', '')
+        t = t.replace('</b>', '')
+        i['title'] = t
+        i['isbn'] = i['isbn'][11:]
+    return book
 
 if __name__ == "__main__":
     title = input("도서명을 입력하세요 : ")
