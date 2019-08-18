48d39a5c35f5ca86257ac5f300feb2a8d23e8d3e
Neung0 <Neung0@users.noreply.github.com>
2019-08-19 00:49:37 +0900

    create model about store information

--- search/admin.py
+++ search/admin.py
@@ -1,1 +1,4 @@
 from django.contrib import admin
+from search.models import Store
+
+admin.site.register(Store)
--- search/models.py
+++ search/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+
+class Store(models.Model):
+    place_name = models.CharField(max_length=50)
+    road_address_name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    in_seoul = models.BooleanField()
+    x = models.CharField(max_length=20)
+    y = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.place_name
