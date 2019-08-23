d7adf0b362a2cda3044e20e6fa7dc6a776d2c0f1
Neung0 <Neung0@users.noreply.github.com>
2019-08-23 19:01:07 +0900

    set click event on th

---
+++ search/static/js/func.js
@@ -0,0 +1,19 @@
+// data = 지점 정보, table = 자료를 입력할 테이블
+var drawTable = function (data, table) {
+    t = 0; // 지점 수를 체크하기 위한 변수
+    // 8개 열이 들어가는 행 생성을 위한 for문
+    for(i=0; i<(data.length/8); i++){
+        var row1= $("<tr />"); // 지점명 행(th)
+        var row2= $("<tr />"); // 재고 행(td)
+        $(table).append(row1);
+        $(table).append(row2);
+        // 8개 열에 각 지점, 재고 정보 입력 위한 for문
+        for(j=0; j<8; j++){
+            // t가 지점 수 보다 클 경우 for문 종료
+            if (t > (data.length-1)) break;
+            row1.append($("<th>" + data[t].fields.place_name + "</th>"));
+            row2.append($("<td>" + data[t].fields.stock + "</td>"));
+            t++;
+        }
+    }
+}
--- search/static/js/map.js
+++ search/static/js/map.js
@@ -5,25 +5,28 @@ var positions = new Array(); // 지점 정보를 담을 배열
 var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(37.535442, 126.9883856), // 지도의 중심좌표
-        level: 8 // 지도의 확대 레벨
+        level: 8 // 지도 확대 레벨
     };
 var map = new kakao.maps.Map(mapContainer, mapOption); // 지도 생성  
 
+// positions에 각 지점 정보 입력위한 for문
 for (var i = 0; i < store.length; i++){
     var p = new Object(); // positions에 담을 객체 생성
+    // fields는 각 지점의 실질적인 정보를 담고있는 dict
     p.title = store[i].fields.place_name;
     p.road_address_name = store[i].fields.road_address_name;
     p.phone = store[i].fields.phone;
     p.latlng = new kakao.maps.LatLng(Number(store[i].fields.y), Number(store[i].fields.x));
     p.stock = store[i].fields.stock;
-    positions.push(p)
+    p.pk = store[i].pk;
+    positions.push(p);
 }
 
 for (var i = 0; i < positions.length; i ++) {
     if (positions[i].stock == 0){
         continue;
     }
-    // 마커를 생성합니다
+    // 마커 생성
     var marker = new kakao.maps.Marker({
         map: map, // 마커를 표시할 지도
         position: positions[i].latlng, // 마커를 표시할 위치
@@ -32,6 +35,33 @@ for (var i = 0; i < positions.length; i ++) {
 }
 
 marker.setMap(map); // 마커 지도 위에 표시
+
+$(".stock_table").on("click", "th", function() {
+    var place = $(this).text();
+    $.each(positions, function(index, item) {
+        if (item.title == place){
+            alert(item.pk);
+            return;
+        }
+    });
+});
+// 클릭 시
+// function getStore(place){
+    
+// };
+    
+
+
+function panTo() {
+    // 이동할 위도 경도 위치를 생성합니다 
+    var moveLatLon = new kakao.maps.LatLng(33.450580, 126.574942);
+    
+    // 지도 중심을 부드럽게 이동시킵니다
+    // 만약 이동할 거리가 지도 화면보다 크면 부드러운 효과 없이 이동합니다
+    map.panTo(moveLatLon);            
+}     
+
+
 
 var p = JSON.stringify(positions);
 document.write(p);
