6cc775e382fb6916112b9c38e9414e560bf2a962
yachoo <dpcksdl3@naver.com>
2020-06-21 22:56:14 +0900

    최직

--- blogapp/models.py
+++ blogapp/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 
 
 class Blog(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    # author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField('date published')
     image = models.ImageField(upload_to='images/', null=True, blank=True)
